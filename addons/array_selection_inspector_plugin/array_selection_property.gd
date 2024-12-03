b1b11605ac9f75048a52ec9555096e81fd480d59
ArokhSlade <139560570+ArokhSlade@users.noreply.github.com>
2024-12-03 21:58:20 +0100

    shenanigan: unwind through the editor

--- addons/array_selection_inspector_plugin/array_selection_property.gd
+++ addons/array_selection_inspector_plugin/array_selection_property.gd
@@ -3,7 +3,16 @@ extends EditorProperty
 var gui_scene : PackedScene = preload("res://addons/array_selection_inspector_plugin/array_select.tscn")
 
 func _init():
+	pass
+
+func _ready():
 	var control = gui_scene.instantiate()
 	#var control = Button.new()
-	add_child(control)
+	get_parent_control().add_child(control)
 	add_focusable(control)
+	var parent = get_parent()
+	var parent_control = get_parent_control()
+	while(parent):
+		print(parent.get_property_list())
+		parent = parent.get_parent()
+	
