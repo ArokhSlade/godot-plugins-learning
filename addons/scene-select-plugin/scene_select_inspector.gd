12afbe8120fad69dd9ed903e843341c838bb65f3
ArokhSlade <139560570+ArokhSlade@users.noreply.github.com>
2024-12-11 14:13:12 +0100

    geting the list during parse property

--- addons/scene-select-plugin/scene_select_inspector.gd
+++ addons/scene-select-plugin/scene_select_inspector.gd
@@ -11,18 +11,30 @@ func _can_handle(object : Object):
 	return true
 
 var found_scene_list = false
+var scene_list : SceneList
+
 func _parse_property(object, type, name, hint_type, hint_string, usage_flags, wide):
-	if name == "scene_list.gd" && usage_flags == "PROPERTY_USAGE_CATEGORY":
-		found_scene_list = true #TODO ...?
+	if name == "scene_select.gd" && usage_flags == "PROPERTY_USAGE_CATEGORY":		
+		print("found scene_select.gd category")		
+	if name == "scene_list":
+		print("found scene list")
+		found_scene_list = true	
+		scene_list = object.get(name)		
 	if name == "selected_scene" && hint_string == "PackedScene":
-		pass #TODO...
-	if type == TYPE_OBJECT && hint_string == "SceneSelect":
+		print("scene list: ", scene_list)
 		var property = object.get(name)
-		print ("object: ", object)		
-		print("property: ", property)
-		
-		property_instance = scene_select_property.new(property)
-		add_property_editor(name, property_instance)		
+		print("Property: ", property)
+		property_instance = scene_select_property.new(property, scene_list)
+		add_property_editor(name, property_instance)
+		#add_property_editor_for_multiple_properties("list and choice",["selected_scene","scene_list","selection_index","fake_property"],property_instance)
+		found_scene_list = false
+	#if type == TYPE_OBJECT && hint_string == "SceneSelect":
+		#var property = object.get(name)
+		#print ("object: ", object)		
+		#print("property: ", property)
+		#
+		#property_instance = scene_select_property.new(property)
+		#add_property_editor(name, property_instance)		
 		#add_property_editor_for_multiple_properties("multiple_things", [name,"options_array"], property_instance)
 		#return true #return false / nothing to let godot show default ui in addition to ours
 
--- addons/scene-select-plugin/scene_select_property.gd
+++ addons/scene-select-plugin/scene_select_property.gd
@@ -10,9 +10,12 @@ var current_value : SceneSelect
 
 var updating = false #NOTE(ArokhSlade,2024 12 09) following godot docs guidance here, does not make sense to me
 
-
-func _init(property : SceneSelect):
+func _init(property : SceneSelect, _scene_list : SceneList):	
 	current_value = property
+	print("current value: ", current_value)
+	print("scene list: ", _scene_list)
+	scene_list = _scene_list
+	
 	init_ui()
 	
 	
@@ -21,8 +24,8 @@ func init_ui():
 	
 	add_child(gui)
 	
-	#for option in scene_list.names:
-		#option_button.add_item(option)
+	for option in scene_list.names:
+		option_button.add_item(option)
 	
 	option_button.item_selected.connect(on_item_selected)
 	
@@ -36,9 +39,8 @@ func on_item_selected(value):
 	if updating:
 		return
 	
-	selected_enemy = get_enemy_for_index(value)		
-	current_value = SceneSelect.new() # TODO: dont't reallocate
-	current_value.enemy_scene = selected_enemy
+	selected_enemy = get_enemy_for_index(value)
+	current_value.selected_scene = selected_enemy
 	current_value.scene_index = value
 	emit_changed(get_edited_property(),current_value)
 
@@ -47,10 +49,10 @@ func get_enemy_for_index(index : int):
 	return scene_list.scenes[index]
 
 
-func _update_property():	
-	pass
-	#var new_value : SceneSelect = get_edited_object()[get_edited_property()]
-	#
-	#updating = true	
-	#current_value = new_value
-	#updating = false
+func _update_property():
+	
+	var new_value : SceneSelect = get_edited_object()[get_edited_property()]
+	
+	updating = true	
+	current_value = new_value
+	updating = false
