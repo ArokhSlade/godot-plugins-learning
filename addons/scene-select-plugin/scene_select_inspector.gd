0edbf80f3d2aaba0593737e85f1cd317739dc8cd
ArokhSlade <139560570+ArokhSlade@users.noreply.github.com>
2024-12-23 19:58:53 +0100

    approach 5: 3, but with drop-down menu in user inspector

--- addons/scene-select-plugin/scene_select_inspector.gd
+++ addons/scene-select-plugin/scene_select_inspector.gd
@@ -3,11 +3,12 @@ extends EditorInspectorPlugin
 
 var scene_select_property = preload("res://addons/scene-select-plugin/scene_select_property.gd")
 var scene_select_property_4 = preload("res://addons/scene-select-plugin/scene_select_property_4.gd")
+var scene_select_property_5 = preload("res://addons/scene-select-plugin/scene_select_property_5.gd")
 var property_instance
 var scene_select : SceneSelect
 
 func _can_handle(object : Object):
-	#PluginDevHelpers.print_props(object)	
+	PluginDevHelpers.print_props(object)	
 	if object is SceneSelect:		
 		return true
 	return true
@@ -21,6 +22,9 @@ func _parse_property(object, type, name, hint_type, hint_string, usage_flags, wi
 	if type == TYPE_OBJECT && hint_type == PROPERTY_HINT_RESOURCE_TYPE && hint_string == "SceneSelect4":
 		print("FOUND version 4")
 		property_instance = scene_select_property_4.new(object.get(name), object)
-		add_property_editor(name, property_instance)
-		
+		add_property_editor(name, property_instance)	
+	if type == TYPE_OBJECT && hint_string == "SceneSelect":
+		print("FOUND SceneSelect as property, making Ver.5 editor")
+		property_instance = scene_select_property_5.new(object.get(name), object)
+		add_property_editor(name, property_instance)		
 	pass 
